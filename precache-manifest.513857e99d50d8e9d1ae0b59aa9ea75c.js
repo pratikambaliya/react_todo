cd69509cde66076ac1078e5592f9840c51c60e72
pratikambaliya <pratikambaliya98@gmail.com>
2020-01-20 14:40:04 +0530

    Updates

---
+++ precache-manifest.513857e99d50d8e9d1ae0b59aa9ea75c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d7d6918242f706c57cabf52fa832b3",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "33f7e3428249d7c7e882",
+    "url": "/react_todo/static/css/main.bee83ffc.chunk.css"
+  },
+  {
+    "revision": "6ecf24a68dcef1ac70a7",
+    "url": "/react_todo/static/js/2.2af9e1f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_todo/static/js/2.2af9e1f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33f7e3428249d7c7e882",
+    "url": "/react_todo/static/js/main.a1b51140.chunk.js"
+  },
+  {
+    "revision": "45318cf2231adcab36b0",
+    "url": "/react_todo/static/js/runtime-main.6ec954e7.js"
+  }
+]);
